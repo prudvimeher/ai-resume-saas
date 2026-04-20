96e64c748758bd7f4e739606cfe84582a49cd6f3
prudvimeher <146476881+prudvimeher@users.noreply.github.com>
2026-04-20 12:12:06 +0530

    resolved cross origin error

--- backend/main.py
+++ backend/main.py
@@ -24,6 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://ai-resume-saas-8ky8ey4vk-gprudvimehers-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
