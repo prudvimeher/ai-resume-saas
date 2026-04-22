e25a8c0ad3e0fd736b22e457d288d76539ecd6da
prudvimeher <146476881+prudvimeher@users.noreply.github.com>
2026-04-22 10:03:25 +0530

    added one more link to CORS

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://ai-resume-saas-8ky8ey4vk-gprudvimehers-projects.vercel.app",
-        "https://ai-resume-saas-wheat.vercel.app/",
+        "https://ai-resume-saas-wheat.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
